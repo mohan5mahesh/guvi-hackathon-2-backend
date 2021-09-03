d0abc40791187470b86a079bda2f9642b53a783d
Mohan <mohan5mahesh@gmail.com>
2021-09-03 23:41:55 +0530

    adding route

--- routes/products.js
+++ routes/products.js
@@ -2,19 +2,11 @@ import express from "express";
 import { getAllProducts, getProductById } from "../helper.js";
 const router = express.Router();
 
-// app.get("/users", async (request, response) => {
-//   const users = await getAllUsers();
-//   // console.log(result);
-//   response.send(users);
-// });
-
-//get all users
 router.get("/", async (request, response) => {
   const productList = await getAllProducts();
   response.send(productList);
 });
 
-//get particular user in users
 router.get("/:productid", async (request, response) => {
   const { productid } = request.params;
   const productList = await getProductById(productid);
